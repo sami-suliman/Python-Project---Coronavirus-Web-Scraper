1a8e3a7c1dd961c2a5eb6204c71d363da6fa6693
sami-suliman <sami.bco@gmail.com>
2020-12-13 06:04:16 -0600

    Create class 'Data' and add init method

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ RUN_TOKEN = "tWX1Tu2g9JAW"
 response = requests.get(f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data', params={"api_key": API_KEY})
 data = json.loads(response.text)
 
+class Data:
+    def __init__(self, api_key, project_token):
+        self.api_key = api_key
+        self.project_token = project_token
+        self.params = {
+            "api_key": self.api_key
+        }
