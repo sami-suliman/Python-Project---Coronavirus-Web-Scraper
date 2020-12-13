46b406cb2d0b41e001c2b836100e1c110cd40fac
sami-suliman <sami.bco@gmail.com>
2020-12-13 06:10:54 -0600

    Add 'get_data' method

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ API_KEY = "tYvBkdiiJEVQ"
 PROJECT_TOKEN = "tTJVZUWJFGGq"
 RUN_TOKEN = "tWX1Tu2g9JAW"
 
-response = requests.get(f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data', params={"api_key": API_KEY})
-data = json.loads(response.text)
+
 
 class Data:
     def __init__(self, api_key, project_token):
@@ -15,3 +14,8 @@ class Data:
         self.params = {
             "api_key": self.api_key
         }
+        self.get_data()
+
+    def get_data(self):
+        response = requests.get(f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data', params={"api_key": API_KEY})
+        self.data = json.loads(response.text)
